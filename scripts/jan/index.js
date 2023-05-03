551f455f3e5a5222f445f95efc42a62aafc6c187
Amy <raulytstation@Outlook.com>
2023-05-03 15:12:18 +0000

    init: start making page

---
+++ scripts/jan/index.js
@@ -0,0 +1,28 @@
+const JanuaryPage = document.createElement(
+    <div>
+        <h1>January</h1>
+        <br />
+        <p>
+            I don't know how to start this "Journal" thing that we are making
+            for english class. But yet here we are nonetheless.
+        </p>
+        <p>
+            January was rough, I was really, really distracted with my personal
+            projects I usually have hosted on my github account. For example,
+            I was finishing up our FRC robot's code, because the competition
+            was starting in a few months. So I had to get going and start learning
+            some programming paradigms that Java uses, for instance,
+            classes, inheritance, abstraction, etc... And learning all of that
+            in a very short amount of time is a daunting task to say the least.
+        </p>
+        <p>
+            Nevertheless, we managed to get the robot's basic parts to
+            function properly.
+        </p>
+        <p>
+            <i>Although</i>, at the same time, I was thinking about being trans.
+        </p>
+    </div>
+);
+
+export default JanuaryPage;
