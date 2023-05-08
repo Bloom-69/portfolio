f635e3271e71153bbfd9efe4d53ee4a113c598a0
Amy <raulytstation@Outlook.com>
2023-05-08 22:17:55 +0000

    feat: Finish march article

--- scripts/mar/index.mjs
+++ scripts/mar/index.mjs
@@ -14,7 +14,19 @@ articleContainer.innerHTML = `
         prefix "poke" it has nothing to do with pokemon). The issue was
         that the video element wasn't playing any kind of videos. It took
         me approximately 3 days to find the root cause of the issue, which was
-        missing codecs on my machine (wow).
+        that I was missing codecs on my machine (wow).
+    </p>
+    <p>
+        I also opened a pull request (basically merging code from a repo/branch
+        to another repo/branch) on revkit, a library made for Revolt.Chat clients,
+        in which I made the project make a switch from GitBook to TypeDoc.
+    </p>
+    <p>
+        Finally, I also made another pull request to add a theme to revolt made
+        by another user, who didn't know how to add it for discovery.
+    </p>
+    <p>
+        Overall, it was a pretty productive month for me.
     </p>
 `;
     
