e9309ce1bcb83359f19b8b44ce5e09601d9b2dfe
Amy <raulytstation@Outlook.com>
2023-05-08 22:49:13 +0000

    feat: add links on homepage

--- scripts/home/index.mjs
+++ scripts/home/index.mjs
@@ -4,7 +4,12 @@ const divContainer = document.createElement("div");
 
 divContainer.id = "pager-content";
 
-divContainer.innerHTML = "<h1>Click a section to see it's article...</h1>";
+divContainer.innerHTML = `
+    <h1>Click a section to see it's article...</h1>
+    <p>Source Code: <a href="https://github.com/stationarystation/school-journal">Github</a></p>
+    <a href="https://stationarystation.vercel.app"><code>whoami</code></a>
+    <p>Made with <b>pure</b> HTML, CSS and JS. And with ❤️</p>
+`;
 
 const home = new PageBuilder().setName("Home").setPath("/").setElement(divContainer).build();
 
